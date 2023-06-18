266498f71ff63fbce79284888ac54cbc78cb4723
AlonsoJIC <jalonsojic@gmail.com>
2023-06-18 07:21:10 -0600

    V2- Reformed total para poder hacer migrations.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 //PARA EJECUTAR EN MODO DEV SIEMPRE, CON NPM RUN DEV
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//DEPENDENCIAS DE LA APP Y CORS
 app.use(express.json());
 
-const whiteList = ['http://localhost:8080', 'https://pagina.com', 'http://localhost:3000', 'http://127.0.0.1:5500'];
-const option = {
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
   origin: (origin, callback) => {
-    if(whiteList.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
-    } else{
-      callback(new Error ('no permitido'));
+    } else {
+      callback(new Error('no permitido'));
     }
   }
 }
-app.use(cors());
+app.use(cors(options));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/api/nueva-ruta', (req, res) => {
+app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,12 +1,13 @@
 //FUNCION DE MIDDLEWARES PARA HACER UN LOG CON LOS ERRORES
+const { ValidationError } = require('sequelize');
+const boom = require('@hapi/boom');
+
 function logErrors (err, req, res, next) {
-  console.log('logErrors');
   console.error(err);
   next(err);
 }
 
 function errorHandler(err, req, res, next) {
-  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack,
@@ -21,5 +22,16 @@ function boomErrorHandler(err, req, res, next) {
   next(err);
 }
 
+function ormErrorHandler(err, req, res, next) {
+  if (err instanceof ValidationError) {
+    res.status(409).json({
+      statusCode: 409,
+      message: err.name,
+      errors: err.errors
+    });
+  }
+  next(err);
+}
 
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }
